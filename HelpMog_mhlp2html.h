e151f0c34c378c314cef1a049554090dc306885c
Mark Bishop <github.sealfin@sealfin.com>
2013-11-28 18:54:11 +0000

    Create HelpMog_mhlp2html.h

---
+++ HelpMog_mhlp2html.h
@@ -0,0 +1,10 @@
+#ifndef HelpMog_mhlp2html_h
+
+#define HelpMog_mhlp2html_h
+
+#include "HelpMog_StringStack.h"
+#include "HelpMog_CharacterQueue.h"
+
+void p_HelpMog_mhlp2html( FILE *p_input, FILE *p_output, const char * const p_inputPath, const char * const p_outputPath );
+
+#endif
