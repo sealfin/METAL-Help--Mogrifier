0a5298ca179d41cc9e68c4733e992b624b7589dd
Mark Bishop <github.sealfin@sealfin.com>
2013-11-28 18:53:07 +0000

    Create HelpMog_RemoveWhitespace.h

---
+++ HelpMog_RemoveWhitespace.h
@@ -0,0 +1,9 @@
+#ifndef HelpMog_RemoveWhitespace_h
+
+#define HelpMog_RemoveWhitespace_h
+
+#include <stdio.h>
+
+void p_HelpMog_RemoveExtraneousWhitespaceCharacters( FILE *p_input, FILE *p_output );
+
+#endif
